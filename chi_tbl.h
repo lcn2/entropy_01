615df15451ca149799ad280e43777cf2d9b32071
Landon Curt Noll <github-mail@asthe.com>
2025-04-19 16:12:52 -0700

    improve Share and enjoy!  :-)

--- chi_tbl.h
+++ chi_tbl.h
@@ -54,17 +54,17 @@
  */
 /*
  * Copyright 1987,1988,1989 by Landon Curt Noll.  All Rights Reserved.
- * 
- * Permission to use, copy, modify, and distribute this software and 
- * its documentation for any purpose and without fee is hereby granted, 
+ *
+ * Permission to use, copy, modify, and distribute this software and
+ * its documentation for any purpose and without fee is hereby granted,
  * provided that the above copyright, this permission notice, and the
  * disclaimer below appear in all of the following:
- * 
- * 	* supporting documentation
- * 	* source copies 
- * 	* source works derived from this source
- * 	* binaries derived from this source or from derived source
- * 
+ *
+ *	* supporting documentation
+ *	* source copies
+ *	* source works derived from this source
+ *	* binaries derived from this source or from derived source
+ *
  * LANDON CURT NOLL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
  * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
  * EVENT SHALL LANDON CURT NOLL BE LIABLE FOR ANY SPECIAL, INDIRECT OR
@@ -72,7 +72,15 @@
  * USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
  * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  * PERFORMANCE OF THIS SOFTWARE.
+ *
+ * chongo (Landon Curt Noll) /\oo/\
+ *
+ * http://www.isthe.com/chongo/index.html
+ * https://github.com/lcn2
+ *
+ * Share and enjoy!  :-)
  */
+
 
 /*
  * chi_tbl.h constants
